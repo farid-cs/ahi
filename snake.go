3aafe890fdc0ea3d536de84272646b0986b3737b
Farid <faridfaracli9@gmail.com>
2025-01-04 18:26:30 +0400

    implement snake head and change grid color

--- snake.go
+++ snake.go
@@ -2,32 +2,113 @@ package main
 
 import rl "github.com/gen2brain/raylib-go/raylib"
 
-var grid [10][16]int
+const (
+	EMPTY = 0
+	HEAD = 1
+)
 
-const WINDOW_WIDTH = int32(100 * len(grid[0]))
-const WINDOW_HEIGHT = int32(100 * len(grid))
+const (
+	WIDTH = 16
+	HEIGHT = 10
+	WINDOW_WIDTH = int32(100 * WIDTH)
+	WINDOW_HEIGHT = int32(100 * HEIGHT)
+)
+
+var snake_head struct {
+	row int
+	col int
+}
+
+var velocity struct {
+	x int
+	y int
+}
+
+var grid = [HEIGHT][WIDTH]int {}
+
+func init() {
+	snake_head.row = 4
+	snake_head.col = 7
+	velocity.x = 1
+}
 
 func main() {
 	rl.InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "snake")
 	defer rl.CloseWindow()
 
+	rl.SetTargetFPS(4)
+
 	for !rl.WindowShouldClose() {
+		/* build the greed */
+		for i := range grid {
+			for j := range grid[0] {
+				grid[i][j] = EMPTY
+			}
+		}
+		grid[snake_head.row][snake_head.col] = HEAD
+
 		rl.BeginDrawing()
 
 		rl.ClearBackground(rl.Red)
 
+		/* display the grid */
 		for i := range grid {
 			for j := range grid[0] {
+				var color = rl.Gray
+
+				if grid[i][j] == HEAD {
+					color = rl.Green
+				}
+
 				rl.DrawRectangle(
 					int32(j * 100 + 2),
 					int32(i * 100 + 2),
 					96,
 					96,
-					rl.Green,
+					color,
 				)
 			}
 		}
 
 		rl.EndDrawing()
+
+		/* update state */
+		switch rl.GetKeyPressed() {
+		case rl.KeyUp:
+			if velocity.y != 0 {
+				break
+			}
+			velocity.x = 0
+			velocity.y = -1
+		case rl.KeyDown:
+			if velocity.y != 0 {
+				break
+			}
+			velocity.x = 0
+			velocity.y = +1
+		case rl.KeyLeft:
+			if velocity.x != 0 {
+				break
+			}
+			velocity.x = -1
+			velocity.y = 0
+		case rl.KeyRight:
+			if velocity.x != 0 {
+				break
+			}
+			velocity.x = +1
+			velocity.y = 0
+		}
+
+		snake_head.col += velocity.x
+		snake_head.col %= WIDTH
+		snake_head.row += velocity.y
+		snake_head.row %= HEIGHT
+		if snake_head.row < 0 {
+			snake_head.row = HEIGHT - 1
+		}
+		if snake_head.col < 0 {
+			snake_head.col = WIDTH - 1
+		}
 	}
 }
