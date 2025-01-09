87be9830574a46b82e8e8aeb6d4ee5b56ad7d408
Farid <faridfaracli9@gmail.com>
2025-01-09 17:52:06 +0400

    refactor: use seperate functions to draw a frame, to initialize state and to update state

--- snake.go
+++ snake.go
@@ -32,82 +32,94 @@ func main() {
 
 	rl.SetTargetFPS(6)
 
-init_state:
+	init_state()
+
+	for !rl.WindowShouldClose() {
+		draw_frame()
+		update_state()
+	}
+}
+
+func init_state() {
 	snake = []Vec2{}
 	snake = append(snake, Vec2{y: 4, x: 7})
 	velocity.x = 1
 	velocity.y = 0
 	food.y = rand.IntN(ROW_COUNT)
 	food.x = rand.IntN(COLUMN_COUNT)
+}
 
-	for !rl.WindowShouldClose() {
-		rl.BeginDrawing()
+func draw_frame() {
+	rl.BeginDrawing()
 
-		rl.ClearBackground(BACKGROUND_COLOR)
+	rl.ClearBackground(BACKGROUND_COLOR)
 
-		for i := 1; i != len(snake); i++ {
-			rl.DrawRectangle(int32(snake[i].x * 100),
-				int32(snake[i].y * 100), 100, 100, TAIL_COLOR)
+	for i := 1; i != len(snake); i++ {
+		rl.DrawRectangle(int32(snake[i].x * 100),
+			int32(snake[i].y * 100), 100, 100, TAIL_COLOR)
+	}
+
+	rl.DrawRectangle(int32(snake[0].x * 100),
+		int32(snake[0].y * 100), 100, 100, HEAD_COLOR)
+
+	rl.DrawCircle(int32(food.x * 100 + 50),
+		int32(food.y * 100 + 50), 50.0, FOOD_COLOR)
+
+	rl.EndDrawing()
+}
+
+func update_state() {
+	last_segment := snake[len(snake)-1]
+
+	switch rl.GetKeyPressed() {
+	case rl.KeyUp:
+		if velocity.y != 0 {
+			break
 		}
-
-		rl.DrawRectangle(int32(snake[0].x * 100),
-			int32(snake[0].y * 100), 100, 100, HEAD_COLOR)
-
-		rl.DrawCircle(int32(food.x * 100 + 50),
-			int32(food.y * 100 + 50), 50.0, FOOD_COLOR)
-
-		rl.EndDrawing()
-
-		switch rl.GetKeyPressed() {
-		case rl.KeyUp:
-			if velocity.y != 0 {
-				break
-			}
-			velocity.x = 0
-			velocity.y = -1
-		case rl.KeyDown:
-			if velocity.y != 0 {
-				break
-			}
-			velocity.x = 0
-			velocity.y = +1
-		case rl.KeyLeft:
-			if velocity.x != 0 {
-				break
-			}
-			velocity.x = -1
-			velocity.y = 0
-		case rl.KeyRight:
-			if velocity.x != 0 {
-				break
-			}
-			velocity.x = +1
-			velocity.y = 0
+		velocity.x = 0
+		velocity.y = -1
+	case rl.KeyDown:
+		if velocity.y != 0 {
+			break
 		}
-
-		last_segment := snake[len(snake)-1]
-		for i := len(snake)-1; i != 0; i-- {
-			snake[i] = snake[i-1]
+		velocity.x = 0
+		velocity.y = +1
+	case rl.KeyLeft:
+		if velocity.x != 0 {
+			break
 		}
-
-		snake[0].x += velocity.x
-		snake[0].x += COLUMN_COUNT
-		snake[0].x %= COLUMN_COUNT
-
-		snake[0].y += velocity.y
-		snake[0].y += ROW_COUNT
-		snake[0].y %= ROW_COUNT
-
-		for i := 1; i != len(snake); i++ {
-			if snake[i] == snake[0] {
-				goto init_state
-			}
+		velocity.x = -1
+		velocity.y = 0
+	case rl.KeyRight:
+		if velocity.x != 0 {
+			break
 		}
+		velocity.x = +1
+		velocity.y = 0
+	}
 
-		if snake[0] == food {
-			snake = append(snake, last_segment)
-			food.y = rand.IntN(ROW_COUNT)
-			food.x = rand.IntN(COLUMN_COUNT)
+	for i := len(snake)-1; i != 0; i-- {
+		snake[i] = snake[i-1]
+	}
+
+	snake[0].x += velocity.x
+	snake[0].x += COLUMN_COUNT
+	snake[0].x %= COLUMN_COUNT
+
+	snake[0].y += velocity.y
+	snake[0].y += ROW_COUNT
+	snake[0].y %= ROW_COUNT
+
+	for i := 1; i != len(snake); i++ {
+		if snake[i] == snake[0] {
+			init_state()
+			return
 		}
+	}
+
+	if snake[0] == food {
+		snake = append(snake, last_segment)
+		food.y = rand.IntN(ROW_COUNT)
+		food.x = rand.IntN(COLUMN_COUNT)
 	}
 }
