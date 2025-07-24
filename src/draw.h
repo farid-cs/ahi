53546f8e3f7bcc8fb764adadb8d5b514b018a09b
Farid <faridfarajli86@gmail.com>
2025-07-24 23:07:28 +0400

    refactor: use brace initialization

--- src/draw.h
+++ src/draw.h
@@ -21,13 +21,13 @@
 
 #include "world.h"
 
-constexpr auto Factor = 100;
+constexpr auto Factor {100};
 
-constexpr auto LineWidth = 1;
-constexpr auto GridWidth = ColumnCount*Factor + (ColumnCount-1)*LineWidth;
-constexpr auto GridHeight = RowCount*Factor + (RowCount-1)*LineWidth;
+constexpr auto LineWidth {1};
+constexpr auto GridWidth {ColumnCount*Factor + (ColumnCount-1)*LineWidth};
+constexpr auto GridHeight {RowCount*Factor + (RowCount-1)*LineWidth};
 
-constexpr auto FontSize = Factor * 0.75;
+constexpr auto FontSize {Factor * 0.75};
 
 void draw_world(const World &w);
 
