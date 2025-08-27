c253efe3f1b718495db1975a912cae6a5eb0ca8c
Farid <faridfarajli86@gmail.com>
2025-08-27 23:32:36 +0400

    refactor: eliminate runtime global state

--- src/ahi.cpp
+++ src/ahi.cpp
@@ -40,26 +40,33 @@ constexpr auto WindowHeight {GridHeight + ScoreHeight};
 constexpr Milliseconds MIN_STATE_DURATION{200};
 constexpr Milliseconds MIN_FRAME_DURATION{1};
 
-static EventListener el{};
-static TimePoint lastUpdateTime{};
-static TimePoint frameUpdateTime{};
-static World world{};
-
-SDL_Window *window{};
-DrawingContext dc{};
-
-static void setup(void)
+int main(int argc, char *argv[])
 {
+	EventListener el{};
+	TimePoint lastUpdateTime{};
+	TimePoint frameUpdateTime{};
+	World world{};
+	SDL_Window *window{};
+	DrawingContext dc{};
+
+	if (argc > 1) {
+		if (std::strcmp(argv[1], "-v")) {
+			std::println(std::cerr, "{} [-v]", argv[0]);
+			return EXIT_FAILURE;
+		}
+		std::println(std::cerr, "{}", WindowTitle);
+		return EXIT_SUCCESS;
+	}
+
+	/* setup */
 	assert(SDL_Init(0));
 	assert(TTF_Init());
 	window = SDL_CreateWindow(WindowTitle, WindowWidth, WindowHeight, 0);
 	assert(window);
 	dc.init(window);
 	lastUpdateTime = now();
-}
 
-static void run(void)
-{
+	/* run */
 	while (el.listen() && !world.win) {
 		draw(dc, world);
 		frameUpdateTime = now();
@@ -70,28 +77,12 @@ static void run(void)
 		}
 		sleep_until(frameUpdateTime+MIN_FRAME_DURATION);
 	}
-}
 
-static void cleanup(void)
-{
+	/* cleanup */
 	dc.deinit();
 	SDL_DestroyWindow(window);
 	TTF_Quit();
 	SDL_Quit();
-}
 
-int main(int argc, char *argv[])
-{
-	if (argc > 1) {
-		if (std::strcmp(argv[1], "-v")) {
-			std::println(std::cerr, "{} [-v]", argv[0]);
-			return EXIT_FAILURE;
-		}
-		std::println(std::cerr, "{}", WindowTitle);
-		return EXIT_SUCCESS;
-	}
-	setup();
-	run();
-	cleanup();
 	return EXIT_SUCCESS;
 }
